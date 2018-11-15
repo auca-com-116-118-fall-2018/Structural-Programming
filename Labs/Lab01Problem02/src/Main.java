b87dabbf635e996764e330f35d6fc47fd1ccb5ce
Temirbekova Aiza <44082449+temirbekova@users.noreply.github.com>
2018-11-15 22:36:15 +0600

    Update Main.java

--- Labs/Lab01Problem02/src/Main.java
+++ Labs/Lab01Problem02/src/Main.java
@@ -1,17 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-       //First Solution
-//             System.out.println("**********************");
-//             System.out.println("*I like programming!*");
-//             System.out.println("**********************");
-
+        System.out.println("**********************");
+        System.out.println("*I like programming!*");
+        System.out.println("**********************");
     }
-    // Second solution
-    //  System.out.println("**********************\n*hello, world*\n**********************")
-
-    //Third Solution
-    System.out.println(
-        "**********************\n" +
-
-
 }
