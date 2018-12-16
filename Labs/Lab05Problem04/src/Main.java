8b8e539b63a2128e1d71d29cba317ac6a40b6fcc
Temirbekova Aiza <44082449+temirbekova@users.noreply.github.com>
2018-12-16 14:31:55 +0600

    Add files via upload

---
+++ Labs/Lab05Problem04/src/Main.java
@@ -0,0 +1,34 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+    public void settings() {
+        fullScreen();
+    }
+
+    public void setup() {
+        background(0);
+
+        final int CIRCLE_COUNT = 100;
+        final int MIN_DIAMETER = 10;
+        final int MAX_DIAMETER = 50;
+        for (int i = 0; i < CIRCLE_COUNT; ++i) {
+            int x = (int) random(width);
+            int y = (int) random(height);
+            int diameter = (int) random(MIN_DIAMETER, MAX_DIAMETER);
+
+            int red = (int) random(256);
+            int green = (int) random(256);
+            int blue = (int) random(256);
+
+            noStroke();
+            fill(red, green, blue);
+            ellipse(x, y, diameter, diameter);
+        }
+    }
+
+    public static void main(String [] args) {
+        PApplet.main("Main");
+    }
+
+}
