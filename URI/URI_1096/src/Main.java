de64e0e55c18c462a424284d18ec361ffe663a30
Temirbekova Aiza <44082449+temirbekova@users.noreply.github.com>
2018-12-17 16:07:21 +0600

    Add files via upload

---
+++ URI/URI_1096/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String [] args){
+        for (int i = 1; i <= 9; i += 2) {
+            for (int I = i, J = 7; (J >= 5); J--) {
+                System.out.printf("I=%d J=%d%n", I, J);
+            }
+        }
+    }
+}
